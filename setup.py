d47b12724c79059bbcaa5fb0957526d8439ce89d
Fam Awad Louiz Awad Shihata <fam@awadlouis.com>
2025-06-18 17:28:35 +0200

    fix: bug with node script:

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            "opponent_tracker_node = opponent_tracker.opponent_tracker_node:main",
         ],
     },
 )
